ca9bdb214be319793883e3504aad907124516464
Joshua Collins <pc.refuge@gmail.com>
2020-07-28 17:24:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == phrase.uppercase
+  if phrase == phrase.upcase
     puts "I LOVE YOU TOO PUMPKIN!"
   else
     puts "HUH?! SPEAK UP< SONNY!"
